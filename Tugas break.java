654628b2ff0c614e1b84df7b0c5b4b6a1056a08c
alikal11 <56871581+alikal11@users.noreply.github.com>
2019-10-29 20:47:36 +0700

    Add files via upload

---
+++ Tugas break.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas4;
+
+/**
+ *
+ * @author asus
+ */
+public class Tugas4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i = 0;
+        while ( i < 10){
+            System.out.println(i);
+            i++;
+            if (i == 7){
+                break;
+            }
+        }
+    }
+    
+}
